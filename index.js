d7d5554fb0422fed7ef94041648b1550dc239933
al_kh31@yahoo.com <al_kh31@yahoo.com>
2016-05-05 13:04:20 +0200

    index.js fixed boot

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /* jshint node:true */
 'use strict';
 require('coffee-script/register');
-var HoSController = require('./bootstrap');
 
-var hosController = new HoSController()
-hosController.connect()
+module.exports = require('./bootstrap');
